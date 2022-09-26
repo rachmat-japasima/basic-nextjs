004e971bf4a8c93f418cb0aa962f7d0b6d5ad16d
rachmat-japasima <rachmat.japasima7@gmail.com>
2022-09-26 15:09:46 +0700

    init project

---
+++ components/Layout/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from '../Header'
+import Footer from '../Footer'
+import styles from './Layout.module.css'
+import Head from 'next/head'
+
+interface LayoutProps{
+    children : React.ReactNode;
+    pageTitle: string;
+}
+
+export default function Layout(props: LayoutProps) {
+    const {children, pageTitle} = props;
+  return (
+    <>
+    <Head>
+      <title>NextJS Basic | {''} {pageTitle}</title>
+      <meta name="description" content="Website NextJS Basic" />
+    </Head>
+    <div className={styles.container}>
+        <Header />
+        <div className={styles.content}>{children}</div>
+        <Footer />
+    </div>
+    </>
+  )
+}
